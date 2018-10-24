2fe126854fbe416354ea1a520f839461ee35c0e0
wutao <wtlife@qq.com>
2018-10-24 21:46:43 +0800

    add the frontend

--- fabric/copyright/src/main/java/com/wtlife/boot/dao/UserDao.java
+++ fabric/copyright/src/main/java/com/wtlife/boot/dao/UserDao.java
@@ -1,4 +1,18 @@
 package com.wtlife.boot.dao;
 
-public class UserDao {
+import com.wtlife.boot.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface UserDao extends JpaRepository<User, Long> {
+    public List<User> findByName(String name);
+
+    public List<User> findByNameAndPassword(String name, String password);
+
+    @Query
+    public List<User> findUser(@Param("name") String name);
+
 }
--- fabric/copyright/src/main/java/com/wtlife/boot/service/UserService.java
+++ fabric/copyright/src/main/java/com/wtlife/boot/service/UserService.java
@@ -1,4 +1,32 @@
 package com.wtlife.boot.service;
 
+import com.wtlife.boot.dao.UserDao;
+import com.wtlife.boot.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public String registUser(User user) {
+
+        if (userDao.findByName(user.getName()).isEmpty()) {
+            userDao.save(user);
+            return "用户名  " + user.getName() + " 注册成功";
+
+        } else {
+            return "用户名 " + user.getName() + "已被占用！";
+
+        }
+    }
+
+
+    public boolean verifyUser(User user) {
+        if (userDao.findByNameAndPassword(user.getName(), user.getPassword()).isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
